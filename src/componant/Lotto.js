8c6243a554cd1ddea38dc924f8deb5ed6fcc5449
“pcat332” <kasira.cat@gmail.com>
2023-04-02 18:30:57 +0700

    my first pratice react app

--- src/componant/Lotto.js
+++ src/componant/Lotto.js
@@ -2,46 +2,45 @@ import React , {useState } from "react";
 import './Lotto.css';
 
 
-
-
 function Lotto() {
-    const [evStart, setEvStart] = useState(true);
+    const [eventStart, setEventStart] = useState(true);
+    function eventChange() {
+        setEventStart(false);
+    };
+
+
     const [numbers, setNumbers] = useState([0,0,0]);
-    const [numbersBox, setNumbersBox] = useState([[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9]]);
     function randomNums() {
-        setNumbers([ranNums(0),ranNums(1),ranNums(2)]);
+        setNumbers([ranDigi(0),ranDigi(1),ranDigi(2)]);
         return numbers
     };
 
-    function evChange() {
-        setEvStart(false);
-    };
-
-    function exRandomNums() {
-        setNumbersBox([delNums(randomNums(0),0),delNums(randomNums(1),1),delNums(randomNums(2),2)]);
+    const [numbersBox, setNumbersBox] = useState([[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9]]);
+    function reRollLotto() {
+        setNumbersBox([chkDupNums(randomNums(),0),chkDupNums(randomNums(),1),chkDupNums(randomNums(),2)]);
         return numbersBox
     };
 
-    function delNums(delNum,i) {
+    function chkDupNums(delNum,i) {
         let arrBox = numbersBox[i];
         if ( arrBox.length <= 1) {
             if ( i===1){
-            alert("out of number, please reset number");}
-            console.log("i = ",i,"curNums = ",delNum[i]," = ",numbersBox[i])
+            alert("บุญธรรม : หวยเลขหมด เติมเลขหน่อย!");}
+            console.log("digi = ",i,"lottoNums = ",delNum[i]," = ",numbersBox[i])
             return numbersBox[i];
         } else {
-        if (evStart === true)
+        if (eventStart === true)
         {
-            evChange();
+            eventChange();
         } else {
         arrBox.splice(arrBox.indexOf(delNum[i]), 1);
-        console.log("i = ",i,"curNums = ",delNum[i]," = ",numbersBox[i])
+        console.log("digi = ",i,"lottoNums = ",delNum[i]," = ",numbersBox[i])
         }
         }
         return arrBox;
     };
 
-    function ranNums(i) {
+    function ranDigi(i) {
         let curNums = Math.floor(Math.random() * 10);
         let criteria = numbersBox[i].includes(curNums);
         do
@@ -52,7 +51,7 @@ function Lotto() {
         return curNums;
     };
 
-    function resetNums()
+    function resetLotto()
     {
         setNumbers([0 , 0 , 0])
         setNumbersBox([[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9]]);
@@ -60,7 +59,7 @@ function Lotto() {
 
     return (
         <>
-            <img className="ourCat" src={require('./boontham.jpg')} width='200px' alt='Boontham!' />
+            <img className="ourCat" src={require('./asset/boontham.jpg')} width='200px' alt='Boontham!' />
             <h1 className="lotto-nav">บุญธรรมใบ้หวย3ตัวท้าย V.1</h1>
             <div className="lotto-container">
                 <p className="lotto-number" digi={0}>{numbers[0]}</p>
@@ -68,8 +67,8 @@ function Lotto() {
                 <p className="lotto-number" digi={2}>{numbers[2]}</p>
             </div>
             <div>
-            <button className="lotto-button" onClick={exRandomNums}>ให้เปียกแลกหวย</button>
-            <button className="lotto-button" onClick={resetNums}>เลขหมดเติมหน่อย</button>
+            <button className="lotto-button" onClick={reRollLotto}>ให้เปียกแลกหวย</button>
+            <button className="lotto-button" onClick={resetLotto}>ให้แห้งเติมเลข</button>
             </div>
         </>
     );
